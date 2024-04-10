453dfc41f5a502bf3f72bcbfbf1138aa141ad989
thisisteey <dadataiwo82@yahoo.com>
2024-04-10 01:29:10 +0100

    Script that writes a recursive function that queries the Reddit API, parses the title of all hot articles, and prints a sorted count of given keywords

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -7,7 +7,34 @@ REDDIT_URL = "https://www.reddit.com"
 """Base API URL for Reddit"""
 
 
-def count_words(subreddit, word_list, inst={}, after="", cnt=0):
+def print_sorted_histogram(word_counts={}):
+    '''Sorts and prints the given word counts histogram.
+    '''
+    word_counts = list(filter(lambda kv: kv[1], word_counts))
+    word_counts_dict = {}
+    for item in word_counts:
+        if item[0] in word_counts_dict:
+            word_counts_dict[item[0]] += item[1]
+        else:
+            word_counts_dict[item[0]] = item[1]
+    word_counts = list(word_counts_dict.items())
+    word_counts.sort(
+        key=lambda kv: kv[0],
+        reverse=False
+    )
+    word_counts.sort(
+        key=lambda kv: kv[1],
+        reverse=True
+    )
+    result_str = '\n'.join(list(map(
+        lambda kv: '{}: {}'.format(kv[0], kv[1]),
+        word_counts
+    )))
+    if result_str:
+        print(result_str)
+
+
+def count_words(subreddit, word_list, word_counts=[], cnt=0, after=None):
     """gets the count of given words found in hot posts of a subreddit"""
     reddit_headers = {
             "Accept": "application/json",
@@ -17,41 +44,37 @@ def count_words(subreddit, word_list, inst={}, after="", cnt=0):
                 "Safari/17.4.1"
             ])
     }
-    parms = {
-        "after": after,
-        "count": cnt,
-        "limit": 100
-    }
+    srt = "hot"
+    lmt = 30
     res_data = get(
-        f"{REDDIT_URL}/r/{subreddit}/hot.json",
+        f"{REDDIT_URL}/r/{subreddit}.json?sort={srt}&limit={lmt}"
+        f"&count={cnt}&after={after if after else ''}",
         headers=reddit_headers,
-        params=parms,
         allow_redirects=False
     )
-    try:
-        api_res = res_data.json()
-        if res_data.status_code == 404:
-            raise Exception
-    except Exception:
-        print("")
-        return
-    api_data = api_res.get("data")
-    after = api_data.get("after")
-    cnt += api_data.get("dist")
-    for post_data in api_data.get("children"):
-        title_words = post_data.get("data").get("title").lower().split()
-        for wrd in word_list:
-            if wrd.lower() in title_words:
-                wrd_occr = len([x for x in title_words if x == wrd.lower()])
-                if inst.get(wrd) is None:
-                    inst[wrd] = wrd_occr
-                else:
-                    inst[wrd] += wrd_occr
-    if after is None:
-        if len(inst) == 0:
-            print("")
-            return
-        inst = sorted(inst.items(), key=lambda kv: (-kv[1], kv[0]))
-        [print(f"{k}: {v}") for k, v in inst]
+    if not word_counts:
+        word_list = list(map(lambda word: word.lower(), word_list))
+        word_counts = list(map(lambda word: (word, 0), word_list))
+    if res_data.status_code == 200:
+        data = res_data.json()['data']
+        posts = data['children']
+        titles = list(map(lambda post: post['data']['title'], posts))
+        word_counts = list(map(
+            lambda kv: (kv[0], kv[1] + sum(list(map(
+                lambda txt: txt.lower().split().count(kv[0]),
+                titles
+            )))),
+            word_counts
+        ))
+        if len(posts) >= lmt and data['after']:
+            count_words(
+                subreddit,
+                word_list,
+                word_counts,
+                cnt + len(posts),
+                data['after']
+            )
+        else:
+            print_sorted_histogram(word_counts)
     else:
-        count_words(subreddit, word_list, inst, after, cnt)
+        return
