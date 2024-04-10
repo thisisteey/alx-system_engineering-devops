d81f376e1ae4e34b2a996e58d881e768983db2be
thisisteey <dadataiwo82@yahoo.com>
2024-04-10 10:10:59 +0100

    Script that writes a function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -23,7 +23,12 @@ def top_ten(subreddit):
             allow_redirects=False
     )
     if res_data.status_code == 200:
-        for post_item in res_data.json()["data"]["children"][0:10]:
-            print(post_item["data"]["title"])
+        try:
+            data = res_data.json()
+            if data and "data" in data and "children" in data["data"]:
+                for post_item in data["data"]["children"]:
+                    print(post_item["data"]["title"])
+        except Exception as e:
+            pass
     else:
         print(None)
