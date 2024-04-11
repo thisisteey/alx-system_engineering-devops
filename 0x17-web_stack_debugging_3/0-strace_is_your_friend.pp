e6371691ed44abb604e8324ec3f21f8968d43e8f
thisisteey <dadataiwo82@yahoo.com>
2024-04-11 14:19:29 -0700

    Directory that contains web debugging task on using tmux, strace and puppet to find out why Apache is returning a 500 error and correct it

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Corrects path reference issue in WordPress site and restarts Apache
+exec { 'fix-wordpress':
+  command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
+/var/www/html/wp-settings.php; service apache2 restart"',
+  path    => '/usr/bin:/usr/sbin:/bin'
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,2 @@
+# Web stack debugging #3
+This is a directory that contains a debugging task which involves using tools such as tmux, strace and puppet to find out and correct why Apache is returning a 500 error.
