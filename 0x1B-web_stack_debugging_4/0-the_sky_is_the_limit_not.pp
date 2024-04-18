fac9afb42541b7b999a089d1bdabaccb3315609b
thisisteey <dadataiwo82@yahoo.com>
2024-04-18 19:31:43 +0000

    Puppet files that debugs Nginx web server under pressure and mitigates failed requests

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# Debugging Nginx web server under pressure: mitigate failed requests
+exec { 'fix--for-nginx':
+  command => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' \
+/etc/default/nginx; service nginx restart\"",
+  path    => '/usr/bin:/usr/sbin:/bin'
+}
